e8b32c5821c6ec876e4f7dbc96b92ab8545f023f
S <ks26@ms.ds.uhc.com>
2018-05-07 14:49:27 +0530

    adding file creation

--- JenkinsAutomation/src/main/java/com/optum/jenkins/App.java
+++ JenkinsAutomation/src/main/java/com/optum/jenkins/App.java
@@ -1,5 +1,10 @@
 package com.optum.jenkins;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -9,7 +14,17 @@ import org.apache.log4j.Logger;
 public class App {
 	public static final Logger logger = Logger.getLogger(App.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		logger.info("===Jenkins build success===");
+		createTestFile("test-file.txt");
+	}
+
+	private static void createTestFile(String path) throws IOException {
+		// TODO Auto-generated method stub
+		File file = new File(path);
+		FileWriter writer = new FileWriter(file);
+		writer.write("Jenkins Test : "+new Date() + " ==> Writing : " + Math.random());
+		writer.flush();
+		writer.close();
 	}
 }
